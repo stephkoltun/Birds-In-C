fe609122b88ce7bf823a92c6efa34566402420e0
koltunburke-imac <stephkoltun@icloud.com>
2020-01-03 18:15:06 -0500

    assemble into phrases

--- sound-analysis/assemble.py
+++ sound-analysis/assemble.py
@@ -1,49 +1,72 @@
+# STEP THREE: Assemble individual notes into phrases based on In C notation
 from pydub import AudioSegment
 import soundfile as sf
-import pyrubberband as pyrb
+
+
+phrases = [
+    {
+        'phrase': '01',
+        'notes': ['C4_eighth','E4_quarter','C4_eighth','E4_quarter','C4_eighth','E4_quarter']
+    },
+    {
+        'phrase': '02',
+        'notes': ['C4_eighth','E4_eighth','F4_eighth','E4_quarter']
+    },
+    {
+        'phrase': '03',
+        'notes': ['rest_eighth','E4_eighth','F4_eighth', 'E4_eighth']
+    },
+    {
+        'phrase': '06',
+        'notes': ['C5_wholedouble']
+    },
+    {
+        'phrase': '07',
+        'notes': ['rest_quarter','rest_quarter','rest_quarter', 'rest_eighth','C4_eighth','C4_eighth','C4_eighth','rest_eighth','rest_quarter','rest_quarter','rest_quarter','rest_quarter']
+    },
+    {
+        'phrase': '11',
+        'notes': ['F4_sixteenth','G4_sixteenth','B4_sixteenth','G4_sixteenth','B4_sixteenth','G4_sixteenth']
+    },
+    {
+        'phrase': '12',
+        'notes': ['F4_eighth','G4_eighth','B4_whole','C5_quarter']
+    },
+    {
+        'phrase': '13',
+        'notes': ['B4_sixteenth','G4_eighthdotted', 'G4_sixteenth','F4_sixteenth','G4_eighth','rest_eighthdotted','G4_halfdotted'] # THIS ONE ISN'T RIGHT!!
+    },
+    {
+        'phrase': '14',
+        'notes': ['C5_whole','B4_whole','G4_halfdotted', 'G4_whole']
+    },
+    {
+        'phrase': '22',
+        'notes': ['E4_quarterdotted','E4_quarterdotted','E4_quarterdotted','E4_quarterdotted','E4_quarterdotted','F#4-Gb4_quarterdotted','G4_quarterdotted','A4_quarterdotted','B4_eighth']
+    }
+]
+
+bird = 'blue-jay'
+
+for phrase in phrases:
+    phAudio = AudioSegment.empty()
+    for note in phrase['notes']:
+
+        filepath = 'WAV-MP3/' + bird + '/shifted_Notes/' + bird + '_' + note + '.wav'
+        loadedNote = AudioSegment.from_wav(filepath)
+        if ('rest' in note):
+            loadedNote = loadedNote - 100
+        phAudio = phAudio + loadedNote
+    filename = 'WAV-MP3/' + bird + '/' + bird + '_Phrase' + phrase['phrase'] + '.wav'
+    phAudio.export(filename, format="wav")
 
 
 ## INITIAL NOTES MADE FROM ORIGINAL FILE
-yC = AudioSegment.from_wav('owl-C.wav')
-yE = AudioSegment.from_wav('owl-E.wav')
-
-print('c length', yC.duration_seconds)
-print('e length', yE.duration_seconds)
-
-
-## MAKE DIFFERENT LENGTHS
-noteLengths = {
-    'eighth': 0.25,
-    'quarter': 0.52,
-    'half': 1,
-    'dottedhalf': 1.5,
-    'whole': 2,
-}
-
-
-eighthC = None
-quarterE = None
-
-
-if (yC.duration_seconds > noteLengths['eighth']):
-    eighthC = yC[:noteLengths['eighth']*1000]
-else:
-    print('c not long')
-
-
-if (yE.duration_seconds > noteLengths['quarter']):
-    quarterE = yE[:noteLengths['quarter']*1000]
-else:
-    print('e not long')
-    print('initial dur', yE.duration_seconds)
-    print('stretching E')
-    playback = (yE.duration_seconds/noteLengths['quarter'])
-    print('playback', playback)
-    tempY, tempSr = sf.read('owl-E.wav')
-    longE = pyrb.time_stretch(tempY, tempSr, playback)
-    sf.write('owl-E-long.wav', longE, tempSr, subtype='PCM_24')
-    print('new dur', sf.info('owl-E-long.wav').duration)
-
+# yC = AudioSegment.from_wav('owl-C.wav')
+# yE = AudioSegment.from_wav('owl-E.wav')
+#
+# print('c length', yC.duration_seconds)
+# print('e length', yE.duration_seconds)
 
 
 ##### COMPOSE INTO PHRASE
@@ -52,6 +75,6 @@ else:
 # final = eighthC + quarterE + eighthC + quarterE + eighthC + quarterE
 # final.export("owl-phrase1.wav", format="wav")
 #
-# # no crossfade
+# with crossfade
 # finalCross = eighthC.append(quarterE, crossfade=100).append(eighthC, crossfade=100).append(quarterE, crossfade=100).append(eighthC, crossfade=100).append(quarterE, crossfade=100)
 # finalCross.export("owl-phrase1-cross300.wav", format="wav")
